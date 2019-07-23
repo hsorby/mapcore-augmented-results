1baa8fa07748acacf1e24ba347aff200736714d8
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-07-24 07:43:16 +1200

    Initial commit of code.

---
+++ README.rst
@@ -0,0 +1,5 @@
+
+MAPcore Augmented Results
+=========================
+
+An NPM Javascript module for augmenting search results returned from scicrunch.
---
+++ src/index.js
@@ -0,0 +1,8 @@
+exports.mapcore_augmented_results_module = function()  {
+
+  const initialise = () => {
+    console.log("Initialise augmented results.");
+  }
+
+  initialise();
+}
